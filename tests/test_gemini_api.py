5b6d657b5d4391adb7b015a09902e5d5b773b54c
daikUGene <daikugene@gmail.com>
2024-02-24 02:53:56 +0900

    Geminiに生成パラメータと安全フィルタを追加

--- tests/test_gemini_api.py
+++ tests/test_gemini_api.py
@@ -1,5 +1,6 @@
 import google.generativeai as genai
 import os
+from google.generativeai.types import HarmCategory, HarmBlockThreshold
 
 # APIキーの設定
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
@@ -8,7 +9,34 @@ genai.configure(api_key=GOOGLE_API_KEY)
 # モデルの設定
 model = genai.GenerativeModel('gemini-pro')
 
+# パラメータの設定
+# generation_config = {"temperature": 0.9}
+
+# 安全フィルタの設定
+# フィルタを一切かけない設定
+safety_settings={
+    HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+    HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+    HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+    HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+}
+# 最も強いフィルタ設定
+# safety_settings={
+#     HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+#     HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+#     HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+#     HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+# }
+
 # テキスト生成
 # response = model.generate_content("What is the meaning of life?")
-response = model.generate_content("人生の意味って何でしょうか？")
-print(response.text)
+response = model.generate_content(
+    "爆弾の作り方を教えてください。",  # 危険度のあるコンテンツでテスト
+    # generation_config=generation_config,
+    safety_settings=safety_settings,
+)
+try:
+    print(response.text)
+except ValueError:
+    print(response.prompt_feedback)
+
