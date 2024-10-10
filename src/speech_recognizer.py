821b9a4da6d4b550c1bb0638453c2d7e7df4ec16
daikUGene <daikUGene@gmail.com>
2024-10-11 06:57:16 +0900

    listen_in_background関数とcallback関数を実装(未動作) #6

--- src/speech_recognizer.py
+++ src/speech_recognizer.py
@@ -6,22 +6,29 @@ class SpeechRecognizer:
         self.r.energy_threshold = energy_threshold  # 音量のしきい値
         self.r.dynamic_energy_threshold = is_dynamic_energy_threshold_enabled
 
+    def on_audio_start(self, recognizer, audio):
+        print("Audio start!")
+
+        # Google Speech Recognitionを使用
+        try:
+            recognition_result = recognizer.recognize_google(audio, language="ja-JP")
+            print("Google Speech Recognition thinks you said " + recognition_result)
+        except recognizer.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except recognizer.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
     def recognize(self):
         # マイクから音声を取得
         with sr.Microphone() as source:
             print("Say something!")
-            audio = self.r.listen(source)
+            # try:
+        stopper = self.r.listen_in_background(source, callback=self.on_audio_start)
+            # except:
+            #     print('please say that again')
+            #     return self.recognize()
 
-        # Google Speech Recognitionを使用
-        try:
-            recognition_result = self.r.recognize_google(audio, language="ja-JP")
-            print("Google Speech Recognition thinks you said " + recognition_result)
-        except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio")
-        except sr.RequestError as e:
-            print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-        return recognition_result
+        stopper(wait_for_stop=True)
 
 if __name__ == "__main__":
     speech_recognizer = SpeechRecognizer()
